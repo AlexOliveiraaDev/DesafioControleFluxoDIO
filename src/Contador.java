9bebdebbc0fda0d452f286a57bcb037ec413b929
Alex Oliveira <contatoalexandergv@gmail.com>
2024-09-23 18:37:52 -0300

    segundo commit

---
+++ src/Contador.java
@@ -0,0 +1,9 @@
+public class Contador {
+    public static Double[] contador(double n1, double n2) throws ParametrosInvalidosException {
+    if (n2 < n1){
+        throw new ParametrosInvalidosException();
+    }
+    else {
+        return null
+    };
+}
